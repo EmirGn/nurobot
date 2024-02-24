b0e462ba5dd0c0ea27857d1bd020518923e47595
EmirG <emirgunl.eg@gmail.com>
2024-02-24 22:46:58 +0300

    added

--- main_script.py
+++ main_script.py
@@ -5,21 +5,26 @@ from preprocessing import *
 from langchain.chat_models import ChatOpenAI
 from langchain_core.runnables import RunnablePassthrough
 from langchain.prompts import ChatPromptTemplate
+from langchain.schema.output_parser import StrOutputParser
 
-llm = ChatOpenAI(model_name = "gpt-3.5-turbo", openai_api_key = OPENAI_API_KEY)
-template = """Answer the question based only on the following context: {context}
+llm = ChatOpenAI(model_name = "gpt-4", openai_api_key = OPENAI_API_KEY)
+template = """Soruları CONTEXTe göre cevapla ve cevapların tek ANA kısımdan oluşacak:
+- Konu Hakkında Direkt Bilgi:
+    UZUN VE DETAYLI CEVAPLAR VER.
+    Sana sorulan sorulara verebildiğin kadar detaylı cevaplar ver. Verilen tüm bilgileri kullan ve istenilen herşeyi ver. 
+    Şartname ile ilgili sorulara kesinlikle doğru cevap ver ve sayısal verilerle cevap vermeye çalış. Kaynak belirt.
+    Örnek raporla ilgili sorulara detaylıca cevap ver ve sayısal verilerin rapordakiyle aynı olduğundan emin ol.
+    Aynı verinin raporda farklı şekillerde kullanıldığını görürsen (roket yarıçap: 1 veya 2) bu sayıların nerelerde kullanıldığını yaz ve kaynak belirt.
+    Matematiksel işlemlere yardım et. Roket yapıyoruz.
 
+    {context}
 Question: {question}
 """
 
+vectordb = load_chunk_persist_pdf()
 prompt = ChatPromptTemplate.from_template(template)
-
+retriever = vectordb.as_retriever()
 # %%
-retriever = vector_db.as_retriever()
-
-# %%
-from langchain.schema.output_parser import StrOutputParser
-
 rag_chain = (
     {"context": retriever, "question": RunnablePassthrough()}
     | prompt
--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,6 @@
 # %%
-import dotenv
 import os
-
-OPENAI_API_KEY = "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"
+import toml
 
 # %%
 import requests
@@ -17,36 +15,44 @@ else:
         f.write(response.text)
 
 # %%
-name1 = "./datasets/teknofest_scheme.pdf"
+with open("./.streamlit/secrets.toml", "r") as f:
+    config = toml.load(f)
+
+OPENAI_API_KEY = config.get("OPENAI_API_KEY")
+if OPENAI_API_KEY:
+    print("Your API Key is available")
+else:
+    print("API key not found")
 
 # %%
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.document_loaders import UnstructuredHTMLLoader
-from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+import chromadb
 
-loader = DirectoryLoader("./datasets")
-pages = loader.load()
+dataset_file = "./datasets"
+documents = []
 
-text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500)
-chunks = text_splitter.split_documents(pages)
+def load_chunk_persist_pdf():
+    for file in os.listdir(dataset_file):
+        if file.endswith(".pdf"):
+            pdf_path = os.path.join(dataset_file, file)
+            loader = PyPDFLoader(pdf_path)
+            documents.extend(loader.load())
 
-# %%
-from langchain.embeddings import OpenAIEmbeddings
-embeddings = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY)
-
-# %%
-import weaviate
-from langchain.vectorstores import Weaviate
-from weaviate.embedded import EmbeddedOptions
-
-client = weaviate.Client(
-    url = "https://muhammed-75p0dubq.weaviate.network",
-    additional_headers = {"openai-key": OPENAI_API_KEY},
-    startup_period = 10,
-)
-
-# %%
-vector_db = Weaviate.from_documents(
-    client = client, documents = chunks, embedding = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY), by_text = False
-)
+        text_splitter = CharacterTextSplitter(chunk_size = 1000, chunk_overlap = 10)
+        chunks = text_splitter.split_documents(documents)
+        client = chromadb.Client()
+        if client.list_collections():
+            consent_collection = client.create_collection("consent_collection")
+        else:
+            print("Collection already exists.")
+        vectordb = Chroma.from_documents(
+            documents = chunks,
+            embedding = OpenAIEmbeddings(openai_api_key = OPENAI_API_KEY),
+            persist_directory = "./vectorss"
+        )
+        vectordb.persist()
+        return vectordb
